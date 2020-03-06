99b87bd9c9f666edc56853d5d0e313e71ba2229a
Eilidh Sweenie <2389212S@student.gla.ac.uk>
2020-03-06 14:35:21 +0000

    Posts models implemented

--- traffic/models.py
+++ traffic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    photo = models.ImageField(upload_to='post_images', blank=True)
+    description = models.CharField(max_length=300)
+    location = models.CharField(max_length=300)
+    dateandtime = models.DateTimeField(auto_now_add=True, blank=True)
+    Category = models.CharField(max_length=30)
+    #CharField(max_length=300)
+    
+    def __str__(self):
+        return self.title
