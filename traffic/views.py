426461d45801a93fbc9a0b0cac490452f0178359
Benedict Anslow <2382379a@student.gla.ac.uk>
2020-03-06 10:55:47 +0000

    0.1 Http response from index given

--- traffic/views.py
+++ traffic/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Traffic Not-My-Jam is Responsive")
