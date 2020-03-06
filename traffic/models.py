1294586cfc64a0dffcee54d6375bd581eedff9a2
Eilidh Sweenie <2389212S@student.gla.ac.uk>
2020-03-06 14:42:55 +0000

    Posts models implemented with slug

--- traffic/models.py
+++ traffic/models.py
@@ -8,7 +8,12 @@ class Posts(models.Model):
     location = models.CharField(max_length=300)
     dateandtime = models.DateTimeField(auto_now_add=True, blank=True)
     Category = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+    
     #CharField(max_length=300)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.title
