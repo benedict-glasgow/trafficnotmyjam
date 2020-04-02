f23422c75fe80ef4c70967a7a5646d67436dfe65
Borislav Kratchanov <2401649K@student.gla.ac.uk>
2020-04-02 12:28:04 +0100

    Cleaned up the maps javascript and fixed a bug where the map wouldn't change zoom levels automatically between views

--- static/js/maps-Bing.js
+++ static/js/maps-Bing.js
@@ -1,14 +1,19 @@
-
+// Function that loads the map
 function GetMap(){
 
-    var pinData; // = {'centre':[55.8554403, -4.3024976], 'posts':[ {'location':[55.8554403, -4.3024976], 'title':'Glasgow'}, ]}
+    var pinData; 
 
+    // Get the data needed to display the map from the html
     var postIDs = $('#map').attr('data-postids');
     var centre = $('#map').attr('data-centre');
-    var zoom = $('#map').attr('data-zoom');
+    var zoom = parseInt( $('#map').attr('data-zoom') );
 
+    // Get the data from the server
     $.get('/traffic/map', { 'postIDs':postIDs, 'centre':centre}, function(data) {
+        // Parse the data sent back from the server
         pinData = JSON.parse(data);
+
+        // Display the main map
         var map = new Microsoft.Maps.Map('#map', { 
             credentials: 'AuJ09npEnmthbBSxKu6teNImwt-6ZIn83affPp-nyUTAfXypqFl12jmF_SFV0IO6',
             center: new Microsoft.Maps.Location(pinData['centre'][0], pinData['centre'][1]),
@@ -16,6 +21,7 @@ function GetMap(){
             zoom: zoom,
         });
 
+        // Add all of the pins (if any)
         for (var i = 0; i < pinData['posts'].length; i++) {
             var location = new Microsoft.Maps.Location( pinData['posts'][i]['location'][0], pinData['posts'][i]['location'][1] );
             var pin = new Microsoft.Maps.Pushpin( location, {
